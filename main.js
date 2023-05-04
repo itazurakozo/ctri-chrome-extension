634ef1207ae176a9491f3ba4aaf9583f7a682948
Jimmy <jimmytgtran@outlook.com>
2023-05-03 18:18:41 -0700

    chuck norris

--- main.js
+++ main.js
@@ -4,33 +4,37 @@ console.log("hello")
 
 
 
-// const sportsUrl = 'https://sportscore1.p.rapidapi.com/sports/1/teams?page=1';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-// 		'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(sportsUrl, options);
-// 	const result = await response.text();
-// 	console.log(result);
-//     contents.innerHTML = result
-// } catch (error) {
-// 	console.error(error);
-// }
-
-//create the html body using ticky points
-//photos: unsplash
-//stockprices
-//cryptoprices
-//weather
-//
-
-`
-<div class = "unsplash">
-    <img src ${imageSource}/>
-</div>
-`
+async function getChuckData() {
+    const url = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        'X-RapidAPI-Key': '7759e0a0c8mshc264a53188fff38p1949a1jsnc9ac66875e22',
+        'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      chuckData = result
+      console.log(chuckData)
+      body.appendChild(chuckHTML())
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  let chuckData = getChuckData()
+  
+  function chuckHTML () {
+    return `
+    <div>
+        <img src = "https://townsquare.media/site/115/files/2022/03/attachment-ChuckNorris.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89"/>
+        <p>${chuckData.value}</p>
+    </div>
+    `
+  } 
+  
+  
